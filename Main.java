610650fe428f8dec63928cdae686f7d073e91231
James Goalder <25690697-JamesGoalder@users.noreply.replit.com>
2024-03-30 02:42:44 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,44 @@
+public class Main {
+public static void main(String[] args) {
+// Exercise 1a
+int x = 3;
+int y = 6;
+System.out.println("The result of adding x and y is: " + (x + y));
+// Exercise 1b
+double z = 2.99;
+System.out.println("The result of adding x, y and z is: " + (x +y + z));
+// Exercise 1c
+boolean variable1 = true;
+boolean variable2 = false;
+System.out.println("Boolean 1: " + variable1);
+System.out.println("Boolean 2: " + variable2);
+// Exercise 1d
+char myvar1 = 65;
+char myvar2 = 66;
+char myvar3 = 67;
+System.out.print(myvar1);
+System.out.print(myvar2);
+System.out.println(myvar3);
+// Exercise 1e
+int high = 67;
+int low = 55;
+System.out.println("> " + (high > low));
+// Exercise 1f
+String firstname = "James";
+String lastname = "Goalder";
+System.out.println(firstname + " " + lastname);
+// Exercise 1g
+String j = "8";
+int a = 4;
+String g = j + a;
+System.out.println(g);
+// Exercise 1h
+System.out.println("> We are the so called \"Vikings\" from the north");
+// Exercise 1i
+int t = 5;
+int s = 3;
+if (t > s){
+  System.out.print("> t is greater than s");
+  }
+}
+}
